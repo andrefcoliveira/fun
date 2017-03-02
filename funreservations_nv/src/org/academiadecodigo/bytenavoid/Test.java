c18798f351ce04dcd51129237aaf84d5ffdf2df7
Armando Bartolo <armando.parreira.bartolo@gmail.com>
2017-03-02 23:22:50 +0000

    added test class and made amazing stuff

---
+++ funreservations_nv/src/org/academiadecodigo/bytenavoid/Test.java
@@ -0,0 +1,49 @@
+package org.academiadecodigo.bytenavoid;
+
+import org.academiadecodigo.bytenavoid.client.Client;
+import org.academiadecodigo.bytenavoid.facility.Facility;
+import org.academiadecodigo.bytenavoid.facility.FacilityType;
+import org.academiadecodigo.bytenavoid.reservation.Reservation;
+import org.academiadecodigo.bytenavoid.util.FileManager;
+import org.academiadecodigo.bytenavoid.util.FileType;
+
+import java.util.LinkedList;
+
+/**
+ * Created by codecadet on 02/03/17.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+
+
+        LinkedList<Object> list = new LinkedList<>();
+
+        /*Reservation reservation = new Reservation(new Client("armando", "Armando", "ARMANDO", "armando@armando", 919191919),
+                new Facility(1, FacilityType.RUNNING, "run bitch", "rua do run", 1234, "poo caralho!!"), 2, 3, 12);
+
+
+        Reservation reservation3 = new Reservation(new Client("armandosetrh", "Armando", "ARMANDO", "armando@armando", 919191919),
+                new Facility(1, FacilityType.RUNNING, "run bitch", "rua do run", 1234, "poo caralho!!"), 2, 3, 12);
+
+
+        Reservation reservation2 = new Reservation(new Client("armandokugkav", "Armando", "ARMANDO", "armando@armando", 919191919),
+                new Facility(1, FacilityType.RUNNING, "run bitch", "rua do run", 1234, "poo caralho!!"), 2, 3, 12);
+
+
+        list.add(reservation);
+        list.add(reservation3);
+        list.add(reservation2);*/
+
+//        FileManager.saveFile(list, FileType.RESERVATION);
+
+        list = FileManager.loadFile(FileType.RESERVATION);
+
+        for (Object obj: list) {
+
+            System.out.println(((Reservation)obj).getClient().getName());
+        }
+
+    }
+}
--- funreservations_nv/src/org/academiadecodigo/bytenavoid/util/FileManager.java
+++ funreservations_nv/src/org/academiadecodigo/bytenavoid/util/FileManager.java
@@ -1,7 +1,67 @@
 package org.academiadecodigo.bytenavoid.util;
 
+import org.academiadecodigo.bytenavoid.client.Client;
+import org.academiadecodigo.bytenavoid.facility.Facility;
+import org.academiadecodigo.bytenavoid.facility.FacilityType;
+import org.academiadecodigo.bytenavoid.reservation.Reservation;
+
+import java.io.*;
+import java.util.LinkedList;
+
 /**
  * Created by codecadet on 01/03/17.
  */
-public class FileManager {
+public abstract class FileManager {
+
+//getClass().getProtectionDomain().getCodeSource().getLocation().toURI().getPath()
+
+    public static void saveFile(LinkedList list, FileType type) {
+
+        try {
+
+            ObjectOutputStream oOS = new ObjectOutputStream(new FileOutputStream(type.getFilename()));
+
+            for (Object obj:list) {
+
+                oOS.writeObject(obj);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static LinkedList<Object> loadFile(FileType type) {
+
+        LinkedList<Object> list = null;
+        try {
+            ObjectInputStream oIS = new ObjectInputStream(new FileInputStream(type.getFilename()));
+
+            list = new LinkedList();
+
+            Object obj;
+
+            while (!(obj = oIS.readObject()).equals(-1)) {
+                list.add(obj);
+            }
+
+            return list;
+
+        } catch (EOFException e) {
+          return list;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
 }
